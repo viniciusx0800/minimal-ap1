fd6f5b0139d861858945a5d6e682744ead8c1c8f
viniciusx0800 <vinioliveira2008y@gmail.com>
2024-10-17 15:06:38 -0300

    feat: acréscimo de um serviço, funcionalidade

--- minimal-api/Program.cs
+++ minimal-api/Program.cs
@@ -1,6 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "ola, ed!");
+
+app.MapPost("/login", static (LoginDTO loginDTO) =>{
+    if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+        return Results.Ok("login com sucesso"); 
+    else
+    {
+        return Results.Unauthorized();
+    }       
+    
+});
+
+
 
 app.Run();
+public class LoginDTO
+{
+    public string Email { get; set; } = default!;
+    public string Senha { get; set; } = default!;
+}
