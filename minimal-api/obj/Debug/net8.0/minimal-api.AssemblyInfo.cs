d68fd7b3330398df04da869fdcff6809b35a39ef
viniciusx0800 <vinioliveira2008y@gmail.com>
2024-10-17 15:35:52 -0300

    feat: criar gitignore

--- minimal-api/obj/Debug/net8.0/minimal-api.AssemblyInfo.cs
+++ minimal-api/obj/Debug/net8.0/minimal-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39a6198ca84f1f8cb33c7de6835de5f4647a7fc8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21ae2cd38107c1b85bf24715f1942d43546377e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
