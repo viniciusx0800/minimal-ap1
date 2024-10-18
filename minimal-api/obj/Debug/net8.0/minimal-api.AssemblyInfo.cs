53b39eecda45f7cf251ba66e3cae3fe052a04949
viniciusx0800 <vinioliveira2008y@gmail.com>
2024-10-18 15:14:58 -0300

    build: dependências externas

--- minimal-api/obj/Debug/net8.0/minimal-api.AssemblyInfo.cs
+++ minimal-api/obj/Debug/net8.0/minimal-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+909fc118abd9a66d6801ff023ace14b2472ad09a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6533a7e7856ddee0407c3e237d55ff0442d3ca99")]
 [assembly: System.Reflection.AssemblyProductAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
