5a1257c2a266652f7bb71c260377d0cddefee12e
viniciusx0800 <vinioliveira2008y@gmail.com>
2024-10-18 16:50:07 -0300

    feat: Adicionar uma public para veiculo

--- minimal-api/Infraestrutura/Db/DbContexto.cs
+++ minimal-api/Infraestrutura/Db/DbContexto.cs
@@ -16,6 +16,7 @@ namespace minimal_api.Infraestrutura.Db
         }
         public DbSet<Administrador> Administradores { get; set; } = default!;
 
+        public DbSet<Veiculo> veiculos { get; set; } = default!;
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Administrador>().HasData(
