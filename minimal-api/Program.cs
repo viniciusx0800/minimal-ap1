c50f759f2c09f098c2ec4f6f8ea06e7e450c554e
viniciusx0800 <vinioliveira2008y@gmail.com>
2024-10-18 16:24:25 -0300

    feat: acréscimo de um serviço, funcionalidade

--- minimal-api/Program.cs
+++ minimal-api/Program.cs
@@ -1,9 +1,14 @@
 using System.Data.Common;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using minimal_api.Dominio.DTOs;
+using minimal_api.Dominio.Entidades;
+using minimal_api.Dominio.Interfaces;
 using minimal_api.Infraestrutura.Db;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<iAdministradorServicos, iAdministradorServicos>();
 
 builder.Services.AddDbContext<DbContexto>(options => {
     options.UseMySql(
@@ -18,8 +23,9 @@ var app = builder.Build();
 
 app.MapGet("/", () => "ola, ed!");
 
-app.MapPost("/login", static (LoginDTO loginDTO) =>{
-    if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+app.MapPost("/login", static ( [FromBody]LoginDTO loginDTO, iAdministradorServicos administradorServicos) =>{
+    // if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+    if(administradorServicos.Login(loginDTO) != null)
         return Results.Ok("login com sucesso"); 
     else
     {
