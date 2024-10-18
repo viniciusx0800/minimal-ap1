4168c9b8097d68a87e93f92c7b2eafab1035e9b2
viniciusx0800 <vinioliveira2008y@gmail.com>
2024-10-18 16:50:26 -0300

    feat: acréscimo de um serviço, funcionalidade

---
+++ minimal-api/Infraestrutura/Servicos/AdministradorServicos.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using minimal_api.Dominio.DTOs;
+using minimal_api.Dominio.Entidades;
+using minimal_api.Dominio.Interfaces;
+using minimal_api.Infraestrutura.Db;
+
+namespace minimal_api.Dominio.Servicos
+{
+    public class AdministradorServicos : iAdministradorServicos
+    {
+        private readonly DbContexto _contexto;
+        public AdministradorServicos(DbContexto contexto)
+        {
+            _contexto = contexto;
+        }
+        public Administrador? Login(LoginDTO loginDTO)
+        {
+            var adm = _contexto.Administradores.Where(a => a.Email == loginDTO.Email && a.Senha == loginDTO.Senha).FirstOrDefault();
+            return  adm;
+
+            
+
+        }
+    }
+}
